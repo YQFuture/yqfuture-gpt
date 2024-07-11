a90e367d464df1237df5a57f5b94f10a3dc2cf7a
“zouxiaodong” <13575232531@163.com>
2024-07-11 13:58:34 +0800

    调整把部分代码位置

--- app/training/cmd/rpc/internal/logic/shoptraining/presettinglogic.go
+++ app/training/cmd/rpc/internal/logic/shoptraining/presettinglogic.go
@@ -10,12 +10,37 @@ import (
 	yqmongo "yufuture-gpt/app/training/model/mongo"
 	"yufuture-gpt/app/training/model/orm"
 	"yufuture-gpt/common/consts"
+	"yufuture-gpt/common/utils"
 )
 
 type PreSettingLogic struct {
 	ctx    context.Context
 	svcCtx *svc.ServiceContext
 	logx.Logger
+}
+
+type ApplyGoodsJsonReq struct {
+	AppType string    `json:"app_type"`
+	SkuList []*string `json:"sku_list"`
+	Channel string    `json:"channel"`
+}
+
+type FetchGoodsJsonReq struct {
+	AppType string `json:"app_type"`
+	Channel string `json:"channel"`
+	Limit   int    `json:"limit"`
+}
+
+type FetchGoodsJsonResp struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+	Data    struct {
+		Items []struct {
+			Sku         string `json:"sku"`
+			CollectTime string `json:"collectTime"`
+			Url         string `json:"url"`
+		} `json:"items"`
+	} `json:"data"`
 }
 
 func NewPreSettingLogic(ctx context.Context, svcCtx *svc.ServiceContext) *PreSettingLogic {
@@ -179,4 +204,88 @@ func UpdateGoodsPreSettingComplete(ctx context.Context, svcCtx *svc.ServiceConte
 		return err
 	}
 	return nil
+}
+
+func ApplyGoodsJson(svcCtx *svc.ServiceContext, trainingGoodsList []*orm.TsGoods) error {
+	var skuList []*string
+	for _, trainingGoods := range trainingGoodsList {
+		skuList = append(skuList, &trainingGoods.PlatformId)
+	}
+	// 发送获取商品JSON请求
+	var applyGoodsJsonResp string
+	err := utils.HTTPPostAndParseJSON(svcCtx.Config.TrainingGoodsConf.ApplyGoodsJsonUrl, ApplyGoodsJsonReq{
+		AppType: "pdd",
+		Channel: svcCtx.Config.TrainingGoodsConf.ApplyGoodsJsonChannel,
+		SkuList: skuList,
+	}, &applyGoodsJsonResp)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// FetchAndSaveGoodsJson 获取获取商品json的url并更新到数据库
+func FetchAndSaveGoodsJson(log logx.Logger, ctx context.Context, svcCtx *svc.ServiceContext, trainingGoodsList []*orm.TsGoods) {
+	i := 0
+	for i < 10 {
+		var fetchGoodsJsonResp FetchGoodsJsonResp
+		// 调用接口
+		err := utils.HTTPPostAndParseJSON(svcCtx.Config.TrainingGoodsConf.FetchGoodsJsonUrl, FetchGoodsJsonReq{
+			AppType: "pdd",
+			Channel: svcCtx.Config.TrainingGoodsConf.ApplyGoodsJsonChannel,
+			Limit:   100,
+		}, &fetchGoodsJsonResp)
+		// 将返回的获取商品json的url更新进mysql
+		for _, item := range fetchGoodsJsonResp.Data.Items {
+			err = svcCtx.TsGoodsModel.UpdateGoodsJsonUrlByPlatformId(ctx, item.Sku, item.Url)
+			if err != nil {
+				log.Error("更新商品json的url失败", err)
+			}
+		}
+		// 查询mysql判断是否完成 同时将完成的新获取json的url保存进training_url
+		complete := true
+		for _, trainingGoods := range trainingGoodsList {
+			// 已经更新获取json的url的商品跳过循环
+			if trainingGoods.GoodsUrl != "" {
+				continue
+			}
+			mysqlGoods, err := svcCtx.TsGoodsModel.FindOne(ctx, trainingGoods.Id)
+			if err != nil {
+				log.Error("查询训练中的商品数据失败", err)
+			} else if mysqlGoods.GoodsJsonUrl == "" {
+				complete = false
+			} else {
+				trainingGoods.GoodsUrl = mysqlGoods.GoodsJsonUrl
+			}
+		}
+		if complete {
+			//都完成后跳出循环
+			break
+		}
+		time.Sleep(time.Minute * 6)
+		i++
+	}
+}
+
+func GetAndParseGoodsJson(log logx.Logger, tsShop *orm.TsShop, goodsDocumentList []*common.PddGoodsDocument, trainingGoodsList []*orm.TsGoods) {
+	for _, trainingGoods := range trainingGoodsList {
+		//根据获取商品JSON的url获取商品JSON 排除掉url为空的商品
+		if trainingGoods.GoodsJsonUrl == "" {
+			continue
+		}
+		var goodsJson string
+		err := utils.HTTPGetAndParseJSON(trainingGoods.GoodsUrl, &goodsJson)
+		if err != nil {
+			log.Error("根据url获取商品json数据失败", err)
+			continue
+		}
+		if goodsJson == "" {
+			log.Error("根据url获取商品json数据失败, 返回的json为空", err)
+			continue
+		}
+
+		// 解析JSON 将图片列表等数据保存下来
+		goodsDocument := ParsePddGoods(goodsJson, tsShop, trainingGoods)
+		goodsDocumentList = append(goodsDocumentList, goodsDocument)
+	}
 }
