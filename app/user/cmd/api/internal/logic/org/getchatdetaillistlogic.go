1fc88acce529c2c01074eebe5a98bd32a1653c3e
“zouxiaodong” <13575232531@163.com>
2024-08-08 19:03:50 +0800

    修复测试发现的问题

--- app/user/cmd/api/internal/logic/org/getchatdetaillistlogic.go
+++ app/user/cmd/api/internal/logic/org/getchatdetaillistlogic.go
@@ -2,7 +2,10 @@ package org
 
 import (
 	"context"
+	"encoding/json"
+	"strconv"
 	"time"
+	"yufuture-gpt/app/user/cmd/rpc/client/org"
 	"yufuture-gpt/common/consts"
 
 	"yufuture-gpt/app/user/cmd/api/internal/svc"
@@ -27,7 +30,90 @@ func NewGetChatDetailListLogic(ctx context.Context, svcCtx *svc.ServiceContext) 
 }
 
 func (l *GetChatDetailListLogic) GetChatDetailList(req *types.ChatDetailReq) (resp *types.ChatDetailResp, err error) {
+	id := l.ctx.Value("id")
+	userId, err := id.(json.Number).Int64()
+	if err != nil {
+		l.Logger.Error("获取用户ID失败", err)
+		return &types.ChatDetailResp{
+			BaseResp: types.BaseResp{
+				Code: consts.Fail,
+				Msg:  "操作失败",
+			},
+		}, nil
+	}
+	shopId, err := strconv.ParseInt(req.ShopId, 10, 64)
+	if err != nil {
+		l.Logger.Error("获取店铺ID失败", err)
+		return &types.ChatDetailResp{
+			BaseResp: types.BaseResp{
+				Code: consts.Fail,
+				Msg:  "操作失败",
+			},
+		}, nil
+	}
+	shopUserId, err := strconv.ParseInt(req.UserId, 10, 64)
+	if err != nil {
+		l.Logger.Error("获取客服用户ID失败", err)
+		return &types.ChatDetailResp{
+			BaseResp: types.BaseResp{
+				Code: consts.Fail,
+				Msg:  "操作失败",
+			},
+		}, nil
+	}
+	buyerId, err := strconv.ParseInt(req.BuyerId, 10, 64)
+	if err != nil {
+		l.Logger.Error("获取客服用户ID失败", err)
+		return &types.ChatDetailResp{
+			BaseResp: types.BaseResp{
+				Code: consts.Fail,
+				Msg:  "操作失败",
+			},
+		}, nil
+	}
 
+	// 调用RPC接口 获取聊天记录
+	chatDetailResp, err := l.svcCtx.OrgClient.GetChatDetail(l.ctx, &org.ChatDetailReq{
+		UserId:     userId,
+		ShopId:     shopId,
+		ShopUserId: shopUserId,
+		BuyerId:    buyerId,
+		StartTime:  req.StartTime,
+		EndTime:    req.EndTime,
+		Query:      req.Query,
+	})
+	if err != nil {
+		l.Logger.Error("获取聊天记录失败", err)
+		return &types.ChatDetailResp{
+			BaseResp: types.BaseResp{
+				Code: consts.Fail,
+				Msg:  "操作失败",
+			},
+		}, nil
+	}
+
+	var data []types.ChatDetail
+	for _, v := range chatDetailResp.List {
+		data = append(data, types.ChatDetail{
+			OwnerId:     strconv.FormatInt(v.OwnerId, 10),
+			SenderType:  v.SenderType,
+			ContentType: v.ContentType,
+			Content:     v.Content,
+			CreateTime:  v.CreateTime,
+		})
+	}
+	/*	return &types.ChatDetailResp{
+		BaseResp: types.BaseResp{
+			Code: consts.Success,
+			Msg:  "操作成功",
+		},
+		Data: data,
+	}, nil*/
+
+	return returnMockChatDetailResp()
+}
+
+func returnMockChatDetailResp() (resp *types.ChatDetailResp, err error) {
 	return &types.ChatDetailResp{
 		BaseResp: types.BaseResp{
 			Code: consts.Success,
--- app/user/cmd/api/internal/logic/org/getshopuserbuyerlistlogic.go
+++ app/user/cmd/api/internal/logic/org/getshopuserbuyerlistlogic.go
@@ -2,8 +2,11 @@ package org
 
 import (
 	"context"
+	"encoding/json"
 	"math/rand"
+	"strconv"
 	"time"
+	"yufuture-gpt/app/user/cmd/rpc/client/org"
 	"yufuture-gpt/common/consts"
 
 	"yufuture-gpt/app/user/cmd/api/internal/svc"
@@ -28,7 +31,70 @@ func NewGetShopUserBuyerListLogic(ctx context.Context, svcCtx *svc.ServiceContex
 }
 
 func (l *GetShopUserBuyerListLogic) GetShopUserBuyerList(req *types.ShopUserBuyerListReq) (resp *types.ShopUserBuyerListResp, err error) {
+	id := l.ctx.Value("id")
+	userId, err := id.(json.Number).Int64()
+	if err != nil {
+		l.Logger.Error("获取用户ID失败", err)
+		return &types.ShopUserBuyerListResp{
+			BaseResp: types.BaseResp{
+				Code: consts.Fail,
+				Msg:  "操作失败",
+			},
+		}, nil
+	}
+	shopId, err := strconv.ParseInt(req.ShopId, 10, 64)
+	if err != nil {
+		l.Logger.Error("获取店铺ID失败", err)
+		return &types.ShopUserBuyerListResp{
+			BaseResp: types.BaseResp{
+				Code: consts.Fail,
+				Msg:  "操作失败",
+			},
+		}, nil
+	}
 
+	// 调用RPC接口 获取店铺客服买家列表
+	l.Logger.Info(userId, shopId)
+	userBuyerListResp, err := l.svcCtx.OrgClient.GetShopUserBuyerList(l.ctx, &org.ShopUserBuyerListReq{
+		UserId:    userId,
+		ShopId:    shopId,
+		StartTime: req.StartTime,
+		EndTime:   req.EndTime,
+		Query:     req.Query,
+	})
+	if err != nil {
+		l.Logger.Error("获取店铺客服买家列表失败", err)
+		return &types.ShopUserBuyerListResp{
+			BaseResp: types.BaseResp{
+				Code: consts.Fail,
+				Msg:  "操作失败",
+			},
+		}, nil
+	}
+
+	var data []types.ShopUserBuyer
+	for _, userBuyer := range userBuyerListResp.List {
+		data = append(data, types.ShopUserBuyer{
+			BuyerId:      strconv.FormatInt(userBuyer.BuyerId, 10),
+			BuyerName:    userBuyer.BuyerName,
+			BuyerHeadImg: userBuyer.BuyerHeadImg,
+			StartTime:    userBuyer.StartTime,
+			AiReturnNum:  userBuyer.AiReturnNum,
+			UsedPower:    userBuyer.UsedPower,
+		})
+	}
+	/*	return &types.ShopUserBuyerListResp{
+		BaseResp: types.BaseResp{
+			Code: consts.Success,
+			Msg:  "操作成功",
+		},
+		Data: data,
+	}, nil*/
+
+	return returnMockShopUserBuyerListResp()
+}
+
+func returnMockShopUserBuyerListResp() (resp *types.ShopUserBuyerListResp, err error) {
 	return &types.ShopUserBuyerListResp{
 		BaseResp: types.BaseResp{
 			Code: consts.Success,
